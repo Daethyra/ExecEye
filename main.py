09575fec68637c4c1bb4fae28afffac0b7365891
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-09-03 23:26:03 -0700

    modified:   .gitignore 	modified:   LICENSE 	modified:   main.py 	new file:   pyproject.toml 	new file:   src/__init__.py 	renamed:    serp_backbone.py -> src/serp_backbone.py 	new file:   src/tests/__init__.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from serp_backbone import DatabaseManager, SerpApiSearch, UserCLI
+""" Start the application. """
+from src.serp_backbone import DatabaseManager, SerpApiSearch, UserCLI
 from cachetools import LRUCache
 from dotenv import load_dotenv
 import os
@@ -7,14 +8,17 @@ load_dotenv()
 
 def main():
     try:
+        # Initialize the database manager, cache, and SerpApiSearch object.
         db_manager = DatabaseManager()
         cache_size = int(os.getenv("CACHE_SIZE", 100))
         cache = LRUCache(maxsize=cache_size)
 
+        # Get the API key from the .env file.
         api_key = os.getenv("SERP_API_KEY")
         if not api_key:
             raise ValueError("API key not found in .env file.")
 
+        # Initialize the CLI and run it.
         serp_api = SerpApiSearch(api_key, db_manager, cache)
         cli = UserCLI(serp_api)
         cli.run()
